94113ed0069d063540b3e22c4e016d9da72487ae
정혜성 <tjdehfldk@gmail.com>
2020-07-27 19:15:48 +0900

    redshift_testing

---
+++ redshift_testing.py
@@ -0,0 +1,30 @@
+import psycopg2
+
+
+def get_redshift_connection():
+    host = "grepp-data.cduaw970ssvt.ap-northeast-2.redshift.amazonaws.com"
+    redshift_user = "tjdehfldk"
+    redshift_pass = "Tjdehfldk1!"
+    port = 5439
+    dbname = "dev"
+    conn = psycopg2.connect("dbname={dbname} user={user} host={host} password={password} port={port}".format(
+        dbname=dbname,
+        user=redshift_user,
+        password=redshift_pass,
+        host=host,
+        port=port
+    ))
+    conn.set_session(readonly=True, autocommit=True)
+
+    return conn.cursor()
+
+
+if __name__ == "__main__":
+    cur = get_redshift_connection()
+
+    sql = "SELECT * FROM raw_data.user_session_channel LIMIT 10;"
+    cur.execute(sql)
+    rows = cur.fetchall()
+
+    for r in rows:
+        print(r)
